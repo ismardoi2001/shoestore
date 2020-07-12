dba3db3abd7ce51e412bbb6ade48c24aa1283025
Muhammad Ismail <ismardoi2001@gmail.com>
2020-07-12 23:47:44 +0500

    update task showing shoes product

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -3,9 +3,19 @@ import Shoes from './../Shoes.json';
 function Product() {
 
 console.log(Shoes)
+console.log(Object.keys(Shoes))
     return (
         <div>
-          <h1>Welcome To Product</h1>  
+          <h1>Welcome To Product</h1> 
+          <div>
+          {Object.keys(Shoes).map(keyName=>{
+            const shoe = Shoes[keyName];
+            return (<div key={keyName}>
+              <h2>{shoe.name}</h2>
+              <img src={shoe.img} height={150}/>
+             </div> )
+          })}
+          </div> 
         </div>
     )
 }
