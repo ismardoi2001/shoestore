f77a2881f69d264e1a2c9c23346a6b1d0b4c0ef7
Muhammad Ismail <ismardoi2001@gmail.com>
2020-07-14 10:27:20 +0500

    update task

--- src/Components/Productitem.js
+++ src/Components/Productitem.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 function Productitem() {
-
+  const { id} = useParams();
+  console.log(id)
 
     return (
         <div>
