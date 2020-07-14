dd8291f642318f270b2557b53d74cfa0feb99892
Muhammad Ismail <ismardoi2001@gmail.com>
2020-07-14 11:24:22 +0500

    update task

--- src/Components/Productitem.js
+++ src/Components/Productitem.js
@@ -7,9 +7,16 @@ function Productitem() {
   const shoe = Shoes[id];
   console.log(shoe)
 
+  if(!shoe)
+   return<h2>Product not Found</h2>
+
     return (
         <div>
-          <h1>Welcome To Productitem</h1>  
+         <h1> Welcome to Product Item</h1>
+         <div  className="link">
+         <h4>{shoe.name}</h4>
+         <img src={shoe.img} height={150}/>
+         </div> 
         </div>
     )
 }
